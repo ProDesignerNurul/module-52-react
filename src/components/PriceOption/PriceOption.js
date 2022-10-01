c5e446663f6d3007966782f62dc5f2f49bc46a8f
prodesignernurul <maxnewsbd@gmail.com>
2022-10-01 18:42:33 +0600

    third commit

--- src/components/PriceOption/PriceOption.js
+++ src/components/PriceOption/PriceOption.js
@@ -1,21 +1,29 @@
 import React from 'react';
-import { CheckCircleIcon } from '@heroicons/react/24/solid'
+import Feature from '../Feature/Feature';
+
 
 const PriceOption = ({ option }) => {
+    const { features } = option;
     return (
         <div>
             <div className='bg-indigo-300 m-4 p-6 rounded-md'>
-                <h2>
-                    <span className='text-8xl'>{option.price}</span>
-                    <span className='text-2xl'>/month</span>
-                </h2>
-                <p className='text-3xl'>{option.name}</p>
-                <div className='flex items-center'>
-                    <CheckCircleIcon className="h-6 w-4 text-purple-800" />
-                    <p className='ml-2'>Awosome Features</p>
+                <div>
+                    <h2>
+                        <span className='text-8xl'>{option.price}</span>
+                        <span className='text-2xl'>/month</span>
+                    </h2>
+                    <p className='text-3xl'>{option.name}</p>
                 </div>
-            </div>
 
+
+                {
+                    features.map((feature, idx) => <Feature
+                    key={idx}
+                    feature={feature}
+                    ></Feature>)
+                }
+                <button className='bg-green-500 w-full rounded-md py-2 text-white font-bold mt-4'>Buy Now</button>
+            </div>
         </div>
     );
 };
--- src/components/Pricing/Pricing.js
+++ src/components/Pricing/Pricing.js
@@ -5,36 +5,36 @@ const Pricing = () => {
     const pricingOptions = [
         { id: 1, name: 'Free', price: 0, features: [
             'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature'
+            'Faluda Feature',
+            'Aluda Feature',
+            'Naluda Feature',
+            'Baluda Feature',
+            'Laluda Feature',
+            'Galuda Feature'
         ] },
         { id: 2, name: 'Free', price: 9.99, features: [
             'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature'
+            'Faluda Feature',
+            'Aluda Feature',
+            'Naluda Feature',
+            'Baluda Feature',
+            'Laluda Feature',
+            'Galuda Feature'
         ] },
         { id: 3, name: 'Free', price: 19.99, features: [
             'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature',
-            'Awsome Feature'
+            'Faluda Feature',
+            'Aluda Feature',
+            'Naluda Feature',
+            'Baluda Feature',
+            'Laluda Feature',
+            'Galuda Feature'
         ] }
     ]
     return (
         <div>
             <h2 className='text-6xl font-bold bg-indigo-300 p-5 mt-4'>Best Deal Of The Town</h2>
-            <div className='grid grid-cols-3 gap-3'>
+            <div className='grid md:grid-cols-3 gap-3 mt-4'>
                 {
                     pricingOptions.map(option => <PriceOption
                         key={option.id}
